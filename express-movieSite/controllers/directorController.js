172e6d0f78e3453d9261fcf36785bb7682ee0439
MatthewLally <mlally8@gmail.com>
2017-11-27 16:32:08 +0000

    Changes

--- express-movieSite/controllers/directorController.js
+++ express-movieSite/controllers/directorController.js
@@ -35,14 +35,54 @@ exports.director_detail = function(req, res, next) {
         
     };
 // Display Director create form on GET
-exports.director_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Director create GET');
+exports.director_create_get = function(req, res, next) {       
+    res.render('director_form', { title: 'Create Director'});
 };
 
-// Handle Director create on POST
-exports.director_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Director create POST');
-};
+// Handle Stars create on POST
+exports.director_create_post = function(req, res, next) {
+    
+     req.checkBody('first_name', 'First name must be specified.').notEmpty(); //We won't force Alphanumeric, because people might have spaces.
+     req.checkBody('second_name', 'second name must be specified.').notEmpty();
+     req.checkBody('date_of_birth', 'Invalid date').notEmpty();
+     req.checkBody('date_of_death', 'Invalid date').optional();
+     req.checkBody('nationality', 'nationality must be specified.').notEmpty();
+     
+     req.sanitize('first_name').escape();
+     req.sanitize('second_name').escape();
+     req.sanitize('first_name').trim();     
+     req.sanitize('second_name').trim();
+     req.sanitize('date_of_birth').toDate();
+     req.sanitize('date_of_death').toDate();
+     req.sanitize('nationality').escape();
+     req.sanitize('nationality').trim();  
+
+ 
+     var errors = req.validationErrors();
+     
+     var director = new Director(
+       { first_name: req.body.first_name, 
+         second_name: req.body.second_name, 
+         date_of_birth: req.body.date_of_birth,
+         date_of_death: req.body.date_of_death,
+         nationality: req.body.nationality
+        });
+        
+     if (errors) {
+         res.render('director_form', { title: 'Create Director', director: director, errors: errors});
+     return;
+     } 
+     else {
+     // Data from form is valid
+     
+         director.save(function (err) {
+             if (err) { return next(err); }
+                //successful - redirect to new author record.
+                res.redirect(director.url);
+             });
+     }
+ 
+ };
 
 // Display Direcor delete form on GET
 exports.director_delete_get = function(req, res) {
--- express-movieSite/controllers/starsController.js
+++ express-movieSite/controllers/starsController.js
@@ -4,14 +4,14 @@ var Movie = require('../models/movie');
 // Display list of all Genre
 exports.stars_list = function(req, res, next) {
     
-      Stars.find({}, 'first_name movies_starred')
-        .populate('first_name')
+      Stars.find()
+        .sort([['first_name', 'ascending']])
         .exec(function (err, list_stars) {
           if (err) { return next(err); }
           //Successful, so render
-          res.render('stars_list', { title: 'Stars List', stars_list: list_stars });
+          res.render('stars_list', { title: 'stars List', list_stars:  list_stars});
         });
-        
+    
     };
 // Display detail page for a specific star
 exports.stars_detail = function(req, res, next) {
@@ -33,14 +33,54 @@ exports.stars_detail = function(req, res, next) {
         
     };
 // Display Stars create form on GET
-exports.stars_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Stars create GET');
+exports.stars_create_get = function(req, res, next) {       
+    res.render('stars_form', { title: 'Create Star'});
 };
 
 // Handle Stars create on POST
-exports.stars_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Stars create POST');
-};
+exports.stars_create_post = function(req, res, next) {
+    
+     req.checkBody('first_name', 'First name must be specified.').notEmpty(); //We won't force Alphanumeric, because people might have spaces.
+     req.checkBody('second_name', 'second name must be specified.').notEmpty();
+     req.checkBody('date_of_birth', 'Invalid date').notEmpty();
+     req.checkBody('date_of_death', 'Invalid date').optional();
+     req.checkBody('nationality', 'nationality must be specified.').notEmpty();
+     
+     req.sanitize('first_name').escape();
+     req.sanitize('second_name').escape();
+     req.sanitize('first_name').trim();     
+     req.sanitize('second_name').trim();
+     req.sanitize('date_of_birth').toDate();
+     req.sanitize('date_of_death').toDate();
+     req.sanitize('nationality').escape();
+     req.sanitize('nationality').trim();  
+
+ 
+     var errors = req.validationErrors();
+     
+     var stars = new Stars(
+       { first_name: req.body.first_name, 
+         second_name: req.body.second_name, 
+         date_of_birth: req.body.date_of_birth,
+         date_of_death: req.body.date_of_death,
+         nationality: req.body.nationality
+        });
+        
+     if (errors) {
+         res.render('stars_form', { title: 'Create Star', stars: stars, errors: errors});
+     return;
+     } 
+     else {
+     // Data from form is valid
+     
+         stars.save(function (err) {
+             if (err) { return next(err); }
+                //successful - redirect to new author record.
+                res.redirect(stars.url);
+             });
+     }
+ 
+ };
 
 // Display Stars delete form on GET
 exports.stars_delete_get = function(req, res) {
